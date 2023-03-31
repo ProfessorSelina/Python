b82c61766069d3dac604eec5ddecf86f96b22666
Selina <mrs.erofa@mail.ru>
2023-03-31 17:24:12 +0300

    1 задача

---
+++ Desktop/PYTHON/Sem1Task2.py
@@ -0,0 +1,13 @@
+# Задача 2: Найдите сумму цифр трехзначного числа.
+
+# 123 -> 6 (1 + 2 + 3)
+# 100 -> 1 (1 + 0 + 0)
+
+def read_data(str):
+    return int(input(str))
+def sum_digit(num1):
+    res = num1 % 10 + num1 // 10 % 10 + num1 // 100 % 10
+    return res
+num = read_data("Введите 3-х значное число: ")
+sum = sum_digit(num)
+print(f"Сумма чисел 3-х значного числа - {num} равна: {sum}")
