6ca5e85395af4b4cf73d12bfe7c9078f6db4c0ba
Selina <mrs.erofa@mail.ru>
2023-05-04 16:47:27 +0300

    11

--- Desktop/JAVA/HW5_1.java
+++ Desktop/JAVA/HW5_1.java
@@ -1,44 +1,53 @@
-import java.util.*;
+package Seminars.Seminar5;
 
-public class PhoneBook {
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class hw5_2 {
     public static void main(String[] args) {
-        HashMap<String, List<String>> phoneBook = new HashMap<>();
-        phoneBook.put("Иван Иванов", Arrays.asList("111-111-111", "222-222-222"));
-        phoneBook.put("Светлана Петрова", Arrays.asList("333-333-333"));
-        phoneBook.put("Кристина Белова", Arrays.asList("444-444-444", "555-555-555"));
-        phoneBook.put("Анна Мусина", Arrays.asList("666-666-666"));
-        phoneBook.put("Анна Крутова", Arrays.asList("777-777-777"));
-        phoneBook.put("Иван Юрин", Arrays.asList("888-888-888"));
-        phoneBook.put("Петр Лыков", Arrays.asList("999-999-999"));
-        phoneBook.put("Павел Чернов", Arrays.asList("000-000-000"));
-        phoneBook.put("Петр Чернышов", Arrays.asList("111-111-111"));
-        phoneBook.put("Мария Федорова", Arrays.asList("222-222-222"));
-        phoneBook.put("Марина Светлова", Arrays.asList("333-333-333"));
-        phoneBook.put("Мария Савина", Arrays.asList("444-444-444"));
-        phoneBook.put("Мария Рыкова", Arrays.asList("555-555-555"));
-        phoneBook.put("Марина Лугова", Arrays.asList("666-666-666"));
-        phoneBook.put("Анна Владимирова", Arrays.asList("777-777-777"));
-        phoneBook.put("Иван Мечников", Arrays.asList("888-888-888"));
-        phoneBook.put("Петр Петин", Arrays.asList("999-999-999"));
-        phoneBook.put("Иван Ежов", Arrays.asList("000-000-000"));
-
-        HashMap<String, Integer> nameCount = new HashMap<>();
-        for (String name : phoneBook.keySet()) {
-            String firstName = name.split(" ")[0];
-            if (nameCount.containsKey(firstName)) {
-                nameCount.put(firstName, nameCount.get(firstName) + 1);
+        Map<String, Integer> nameMap = new HashMap<>();
+        String persons = "Иван Иванов " +
+                "Светлана Петрова " +
+                "Кристина Белова " +
+                "Анна Мусина " +
+                "Анна Крутова " +
+                "Иван Юрин " +
+                "Петр Лыков " +
+                "Павел Чернов " +
+                "Петр Чернышов " +
+                "Мария Федорова " +
+                "Марина Светлова " +
+                "Мария Савина " +
+                "Мария Рыкова " +
+                "Марина Лугова " +
+                "Анна Владимирова " +
+                "Иван Мечников " +
+                "Петр Петин " +
+                "Иван Ежов ";
+        String[] listOfPersons = persons.split(" ");
+        for (int i = 0; i < listOfPersons.length; i += 2) {
+            if (nameMap.containsKey(listOfPersons[i])) {
+                nameMap.replace(listOfPersons[i], nameMap.get(listOfPersons[i]) + 1);
             } else {
-                nameCount.put(firstName, 1);
+                nameMap.put(listOfPersons[i], 1);
             }
         }
-
-        List<Map.Entry<String, Integer>> sortedNames = new ArrayList<>(nameCount.entrySet());
-        sortedNames.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
-
-        for (Map.Entry<String, Integer> entry : sortedNames) {
-            if (entry.getValue() > 1) {
-                System.out.println(entry.getKey() + " - " + entry.getValue() + " раз(а)");
+        Map<String, Integer> sortedNameMap = new LinkedHashMap<>();
+        int max = 1;
+        for (int value : nameMap.values()) {
+            if (value > max) {
+                max = value;
             }
         }
+        for (int i = max; i > 0; i--) {
+            for (Map.Entry<String, Integer> entry : nameMap.entrySet()) {
+                String key = entry.getKey();
+                if (nameMap.get(key) == i) {
+                    sortedNameMap.put(key, nameMap.get(key));
+                }
+            }
+        }
+        System.out.println(sortedNameMap);
     }
 }
