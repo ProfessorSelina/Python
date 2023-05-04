b626d32548f565e91a05be80a891b1a305ebd137
Selina <mrs.erofa@mail.ru>
2023-05-04 16:04:02 +0300

    4

---
+++ Desktop/JAVA/HW2_1.java
@@ -0,0 +1,39 @@
+// В файле содержится строка с исходными данными в такой форме:
+// {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
+// Требуется на её основе построить и вывести на экран новую строку, в форме SQL запроса:
+// SELECT * FROM students WHERE name = "Ivanov" AND country = "Russia" AND city = "Moscow";
+// Для разбора строки используйте String.split. Сформируйте новую строку, используя StringBuilder. Значения null не включаются в запрос.
+package Seminars.Seminar2;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+
+public class hw2_1 {
+    public static void main(String[] args) {
+        Map<String, String> map = new LinkedHashMap<>();
+        map.put("name","Ivanov");
+        map.put("country","Russia");
+        map.put("city","Moskow");
+        map.put("age",null);
+
+        System.out.println(getQuery(map));
+    }
+
+    public static String getQuery(Map<String, String> params) {
+        StringBuilder result = new StringBuilder();
+        if (params == null || params.isEmpty())
+            return result.toString();
+
+        for (Map.Entry<String, String> pair : params.entrySet()) {
+            if (pair.getKey() == null || pair.getValue() == null)
+                continue;
+
+            result.append(pair.getKey()).append(" = '").append(pair.getValue()).append("' and ");
+        }
+
+        if (result.length() > 5)
+            result.delete(result.length() - 5, result.length());
+
+        return result.toString();
+    }
+}
